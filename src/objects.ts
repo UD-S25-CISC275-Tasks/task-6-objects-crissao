dff27dfe8cdda44178b4e9729dfdf32420fc9172
Crissa Owens <159073989+crissao@users.noreply.github.com>
2025-02-21 15:49:59 -0500

    toShortForm done

--- src/objects.ts
+++ src/objects.ts
@@ -45,7 +45,9 @@ export function isValid(question: Question, answer: string): boolean {
  * name "My First Question" would become "9: My First Q".
  */
 export function toShortForm(question: Question): string {
-    return "";
+    const name = question.name.slice(0,10);
+    const id = question.id
+    return `${id}: ${name}`;
 }
 
 /**
